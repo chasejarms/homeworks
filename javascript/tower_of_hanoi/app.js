fc4929aa9c719044c9f209299861bebef882de23
Chase Armstrong <chasejarms@gmail.com>
2017-02-25 17:44:10 -0800

    finish jQuery assignment

---
+++ javascript/tower_of_hanoi/app.js
@@ -0,0 +1,6 @@
+document.write('welcome to my app');
+console.log('app loaded');
+document.write('another thing I wrote');
+console.log('this is working');
+document.write('something else');
+console.log('is this working?');
