29e138379ee6db907d05eaf9f32c780f4ace7abf
Chase Armstrong <chasejarms@gmail.com>
2017-03-05 17:24:55 -0800

    Add currency exchange react/redux application

---
+++ react_projects/currency_exchange/frontend/actions.js
@@ -0,0 +1,11 @@
+
+
+const selectCurrency = (baseCurrency, rates) => (
+  {
+    type: "SWITCH_CURRENCY",
+    baseCurrency: baseCurrency,
+    rates: rates
+  }
+);
+window.selectCurrency = selectCurrency;
+export default selectCurrency;
