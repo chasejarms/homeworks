9bac11ccf6e9f7ff88f67a68b2414a9b75e25f2e
Chase Armstrong <chasejarms@gmail.com>
2017-03-08 06:18:21 -0800

    Practice react routing

---
+++ react_projects/rainbow_routes/components/green.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+
+const Green = () => (
+  <div>
+    <h2 className="green"></h2>
+  </div>
+);
+
+export default withRouter(Green);
---
+++ react_projects/rainbow_routes/components/violet.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+
+const Violet = () => (
+  <div>
+    <h2 className="violet"></h2>
+  </div>
+);
+
+export default withRouter(Violet);
